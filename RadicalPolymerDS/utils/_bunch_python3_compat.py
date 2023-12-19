6d9631915d5b0a0bc4d924bd26da642119888213
Takayoshi Yoshimura <yoshimura485@gmail.com>
2023-12-19 16:33:05 +0900

    Edited to avoid errors under Linux.

--- RadicalPolymerDS/utils/_bunch_python3_compat.py
+++ RadicalPolymerDS/utils/_bunch_python3_compat.py
@@ -13,9 +13,9 @@ else:
         return codecs.unicode_escape_decode(string)[0]
 
 # dict.iteritems(), dict.iterkeys() is also incompatible
-if _IS_PYTHON_3:
-    iteritems = dict.items
-    iterkeys  = dict.keys
-else:
-    iteritems = dict.iteritems
-    iterkeys = dict.iterkeys
+#if _IS_PYTHON_3:
+#    iteritems = dict.items
+#    iterkeys  = dict.keys
+#else:
+#    iteritems = dict.iteritems
+#    iterkeys = dict.iterkeys
